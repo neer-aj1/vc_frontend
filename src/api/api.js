1c3df206798475b50a4d9cf671f644eb40c0a071
Neeraj Kumar <neeraj@recruitcrm.io>
2026-01-19 18:12:51 +0530

    fix env name

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: import.meta.env.BACKEND_BASE_URL || 'http://localhost:5001/api',
+  baseURL: import.meta.env.VITE_BACKEND_BASE_URL || 'http://localhost:5001/api',
   headers: {
     'Content-Type': 'application/json',
   },
